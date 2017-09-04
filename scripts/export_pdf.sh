aeebe07e392a7800236c0941f1b118f89ce8ca23
Mikael Martensson <mikael.martensson@cern.ch>
2017-09-04 14:28:24 +0200

    impreovements to pdf export script

--- scripts/export_pdf.sh
+++ scripts/export_pdf.sh
@@ -1,26 +1,39 @@
-if [[ ! $# = 1 ]]
+#!/bin/bash
+if [[ ! $# = 1 ]] && [[ ! $# = 2 ]]
 then
     echo "Usage:"
-    echo "${0} RECIPE"
+    echo "${0} RECIPE [PDF]"
 else
     recipe=$1
     tmp_dir=export_pdf_tmp
     mkdir -p $tmp_dir
+    echo "mkdir -p ${tmp_dir}"
+    echo $recipe
     # extract recipe name by stripping path
-    name=${recipe/\/*\//}
+    name=${recipe##*/}
+    echo $name
     # also strip extension
     name=${name/.*/}
-    latex=${name/.*/}.tex
-    pdf=${name/.*/}.pdf
+    echo $name
+    latex=${name}.tex
+    echo $latex
+    pdf=${name}.pdf
+    pdf_out=$pdf
+    if [[ $# = 2 ]]
+    then
+	pdf_out=$2
+    fi
+    echo $pdf
     mbrew=./mbrew
     LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../lib/ \
 		   eval $mbrew $recipe --latex $tmp_dir/$latex && \
 	cd $tmp_dir && \
 	lualatex $latex && \
 	lualatex $latex && \
-	mv $pdf ../ && \
+	mv $pdf ../$pdf_out && \
 	cd ../ && \
 	rm -r $tmp_dir
+    echo "rm -r ${tmp_dir}"
 fi
 
 
